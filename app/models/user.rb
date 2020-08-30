d32c598d8642f0b1459c9f1047207ebde8cddbca
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-30 20:45:50 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
-  has_many  :tweets
-
   has_secure_password
+  has_many :tweets
+  validates_presence_of :username, :email, :password_digest
 
   def slug
-    username.downcase.gsub(" ","-")
+    self.username.split(" ").join("-")
   end
 
   def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
+    self.find_by_username(slug.split("-").join(" "))
   end
 
 end
